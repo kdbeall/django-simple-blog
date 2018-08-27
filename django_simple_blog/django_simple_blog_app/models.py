d8796d09f51ae414b0854a585c5846eb2648c681
Daniel Beall <daniel@bnafit.net>
2018-08-27 13:54:57 -0400

    Added a title to Entry model, added test to Entry model, and ran migrations.

--- django_simple_blog/django_simple_blog_app/models.py
+++ django_simple_blog/django_simple_blog_app/models.py
@@ -4,5 +4,10 @@ from django.db import models
 
 
 class Entry(models.Model):
+    title = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
+
     class Meta:
         verbose_name_plural = "entries"
--- django_simple_blog/django_simple_blog_app/tests.py
+++ django_simple_blog/django_simple_blog_app/tests.py
@@ -5,5 +5,9 @@ from .models import Entry
 
 
 class EntryModelTest(TestCase):
+    def test_str(self):
+        entry = Entry(title="My first post!")
+        self.assertEqual(str(entry), entry.title)
+
     def test_verbose_name(self):
         self.assertEqual(str(Entry._meta.verbose_name_plural), "entries")
